7ebdaf4ba4fb07b90e831a667d9e1be70d6ec130
Andrei Maiboroda <amaiboroda@gmail.com>
2016-05-12 20:21:29 +0300

    Add Codility Number Of Disc Intersections problem

---
+++ Codility/6_sorting/number_of_disc_intersections.cpp
@@ -0,0 +1,34 @@
+// https://codility.com/programmers/task/number_of_disc_intersections/
+
+#include <algorithm>
+
+int solution(vector<int> &A) {
+    // y=0 axes intersection x-coordinate and "right side" flag pairs
+    vector<pair<int, bool>> discLimits;
+    for (size_t i = 0; i < A.size(); ++i)
+    {
+        discLimits.push_back(make_pair(i - A[i], false));
+        discLimits.push_back(make_pair(i + A[i], true));
+    }
+    
+    sort(discLimits.begin(), discLimits.end());
+    
+    int intersections = 0;
+    int openDiscs = 0;
+    for (pair<int, bool> limit : discLimits)
+    {
+        if (limit.second)
+        {
+            --openDiscs;
+            intersections += openDiscs;
+            if (intersections > 1e7)
+                return -1;
+        }
+        else
+        {
+            ++openDiscs;
+        }
+    }
+    
+    return intersections;
+}
